b58d2c1d2b2e921ffe67c15cda8a206e7d03fa4f
Mikael Petersen <info@mikaelpetersen.se>
2020-07-12 13:32:04 +0200

    finalized

--- search.js
+++ search.js
@@ -1,11 +1,12 @@
 const formTag = document.querySelector("form");
 const inputTag = formTag.querySelector("input");
+const resultsTag = document.querySelector("section.results");
 
 const accessKey = "CytvQs8dTBPOibHXszVtzgudhsC3ydpwmRaLHrTK8NI";
-const apiUrl = "https://api.unsplash.com/search/photos?per_page=24&query=blue";
+const apiUrl = "https://api.unsplash.com/search/photos?per_page=24&query=";
 
 const searchUnsplash = function (term) {
-  return fetch(apiUrl, {
+  return fetch(apiUrl + term, {
     method: "GET",
     headers: {
       Authorization: "Client-ID " + accessKey,
@@ -13,8 +14,43 @@ const searchUnsplash = function (term) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // format unsplash's results to suis our needs
+      return data.results.map((result) => {
+        return {
+          imageSrc: result.urls.regular,
+          width: result.width,
+          height: result.height,
+          name: result.user.name,
+          title: result.description || "Untitled",
+          backgroundColor: (result.color || "#cccccc") + "33",
+        };
+      });
     });
+};
+
+// Add results to the page
+const addResults = function (results) {
+  // Remove all the loading tags
+  resultsTag.innerHTML = "";
+
+  // Loop over each individual result and add to the results tag
+  results.forEach((result) => {
+    resultsTag.innerHTML =
+      resultsTag.innerHTML +
+      `
+      <div class="single-result">
+        <div class="image" style="background-color: ${result.backgroundColor}">
+          <img src="${result.imageSrc}" />
+        </div>
+        <h2>${
+          result.title.length > 100
+            ? result.title.slice(0, 100).trim() + "..."
+            : result.title
+        }</h2>
+        <p>by ${result.name} — ${result.width} x ${result.height}</p>
+      </div>
+    `;
+  });
 };
 
 // When we submit the form we get what is in the inputTag
@@ -23,7 +59,9 @@ formTag.addEventListener("submit", (event) => {
   const searchTerm = inputTag.value;
 
   // Run a search with the searchTerm
-  searchUnsplash(searchTerm);
+  searchUnsplash(searchTerm).then((results) => {
+    addResults(results);
+  });
 
   // Stop the form from going to the usual next page
   event.preventDefault();
